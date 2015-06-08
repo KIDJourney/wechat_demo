78bfac4e18997b02e8e9d8833b00b49e2f0d85e3
KIDJourney <kingdeadfish@qq.com>
2015-06-08 18:10:37 +0800

    finish user info catch

---
+++ lib/userinfo_class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @Author: kidjourney
+ * @Date:   2015-06-08 16:39:50
+ * @Last Modified by:   kidjourney
+ * @Last Modified time: 2015-06-08 18:09:17
+ */
+require_once("lib/db_common.php");
+require_once("lib/wechat_common.php");
+
+class userinfo{
+    private $user_code;
+    private $user_access_url;
+    private $user_access;
+    private $user_info_url;
+    private $user_info;
+
+    function __construct(){
+        $this->user_code = $_GET['code'];
+        $this->user_access_url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code";
+        $this->user_access_url = sprintf($this->user_access_url,APPID,APPSECRET,$this->user_code);
+    }
+
+    function get_user_access(){
+        $this->user_access = json_decode(file_get_contents($this->user_access_url) , True);
+    }
+
+    function get_user_info(){
+        $user_access_token = $this->user_access['access_token'];
+        $user_access_openid = $this->user_access['openid'];        
+        $this->user_info_url = "https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=zh_CN";
+        $this->user_info_url = sprintf($this->user_info_url,$user_access_token,$user_access_openid);
+        $this->user_info = json_decode(file_get_contents($this->user_info_url));
+    }
+
+    function show(){
+        echo "Your user code ";
+        print_r($this->user_code);
+        echo "<br>Your acess info";
+        print_r($this->user_access);
+        echo "<br>Your info";
+        print_r($this->user_info);
+    }
+
+}
--- userinfo.php
+++ userinfo.php
@@ -3,12 +3,19 @@
  * @Author: kidjourney
  * @Date:   2015-06-08 14:40:35
  * @Last Modified by:   kidjourney
- * @Last Modified time: 2015-06-08 15:08:17
+ * @Last Modified time: 2015-06-08 18:08:39
  */
-require_once("db_common.php");
+require_once("lib/db_common.php");
+require_once("lib/wechat_common.php");
+require_once("lib/userinfo_class.php");
 
 if (!isset($_GET['code'])){
-    die "no direct access right";
+    die ("no direct access right");
 }
 
 
+
+$job = new userinfo();
+$job->get_user_access();
+$job->get_user_info();
+$job->show();
