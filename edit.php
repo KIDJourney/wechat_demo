03f917d499f5481d3f4a145d0443ea6605f42b45
KIDJourney <kingdeadfish@qq.com>
2015-06-09 11:57:52 +0800

    fix nickname modify in session

--- edit.php
+++ edit.php
@@ -3,7 +3,7 @@
  * @Author: kidjourney
  * @Date:   2015-06-09 00:04:26
  * @Last Modified by:   kidjourney
- * @Last Modified time: 2015-06-09 11:45:17
+ * @Last Modified time: 2015-06-09 11:56:38
  */
 require_once("lib/db_common.php");
 require_once("lib/http_common.php");
@@ -22,5 +22,6 @@ if (!isset($_POST['nickname']) or !isset($_POST['info'])){
     $query = sprintf($query , $_POST['nickname'] , $_POST['info'] , $_SESSION['u_openid']);
     $sqliwoker->query($query);
     $_SESSION['u_info'] = $_POST['info'];
+    $_SESSION['u_nick'] = $_POST['nickname'];
     redirect('show.php');    
 }
